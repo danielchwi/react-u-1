40a7b45fd884bc66338c7d17ccfc5c64419f4bcc
danielchwi <daniel.widodo@bitcoin.co.id>
2022-04-12 22:34:33 +0700

    understanding key in list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { useState } from 'react'
 
 const App= () =>  {
   const [name, setName] = useState('Mario')
+  const [events, setEvents] = useState([
+    {name: "Hari ulang tahun bapak", id: 1},
+    {name: "Akhirnya dapat kerja", id: 2},
+    {name: "Beli laptop baru", id: 3}
+  ])
 
   const changeName = () => {
     setName('Luigi')
@@ -13,6 +18,11 @@ const App= () =>  {
     <div className="App">
       <h1>Hallo my name is {name}</h1>
       <button onClick={changeName} >click</button>
+      {events.map((event)=> (
+        <div key={event.id}>
+          <p>{event.name}</p>
+        </div>
+      ))}
     </div>
   );
 }
