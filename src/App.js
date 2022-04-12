0e3c310b7d08c7278de3192fc8470ad97cd5896c
danielchwi <daniel.widodo@bitcoin.co.id>
2022-04-12 23:02:06 +0700

    best practice handling change state

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import './App.css';
 import { useState } from 'react'
 
 const App= () =>  {
-  const [name, setName] = useState('Mario')
   const [events, setEvents] = useState([
     {name: "Hari ulang tahun bapak", id: 1},
     {name: "Akhirnya dapat kerja", id: 2},
     {name: "Beli laptop baru", id: 3}
   ])
 
-  const changeName = () => {
-    setName('Luigi')
-    console.log(name)
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event)=> {
+        return event.id !== id
+      })
+    })
   }
 
   return (
     <div className="App">
-      <h1>Hallo my name is {name}</h1>
-      <button onClick={changeName} >click</button>
       {events.map((event)=> (
         <div key={event.id}>
           <p>{event.name}</p>
+          <button onClick={() => handleClick(event.id)}>delete event</button>
         </div>
       ))}
     </div>
